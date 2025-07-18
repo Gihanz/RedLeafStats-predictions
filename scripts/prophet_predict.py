7ad9ffc0f05595def1991c95b3cdcb803afc8162
GIHAN SHAMIKE <gihanzshamike@gmail.com>
2025-07-18 01:40:55 -0400

    Update prophet_predict.py

--- scripts/prophet_predict.py
+++ scripts/prophet_predict.py
@@ -16,7 +16,6 @@ firebase_creds = {
     "project_id": os.environ["FIREBASE_PROJECT_ID"],
     "private_key": os.environ["FIREBASE_PRIVATE_KEY"].replace("\\n", "\n"),
     "client_email": os.environ["FIREBASE_CLIENT_EMAIL"],
-    "client_id": os.environ["FIREBASE_CLIENT_ID"],
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
     "token_uri": "https://oauth2.googleapis.com/token",
     "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",

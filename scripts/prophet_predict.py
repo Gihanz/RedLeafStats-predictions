5c621ae821169b7532f0ba58092c2982bd314d57
GIHAN SHAMIKE <gihanzshamike@gmail.com>
2025-07-18 02:12:25 -0400

    Update prophet_predict.py

--- scripts/prophet_predict.py
+++ scripts/prophet_predict.py
@@ -7,16 +7,15 @@ from firebase_admin import credentials, firestore
 
 # ✅ Setup Firebase Admin
 firebase_credentials = {
-    "type": "service_account",
+"type": "service_account",
     "project_id": os.environ["FIREBASE_PROJECT_ID"],
-    "private_key_id": os.environ["FIREBASE_PRIVATE_KEY_ID"],
-    "private_key": os.environ["FIREBASE_PRIVATE_KEY"].replace('\\n', '\n'),
+    "private_key": os.environ["FIREBASE_PRIVATE_KEY"].replace("\\n", "\n"),
+
     "client_email": os.environ["FIREBASE_CLIENT_EMAIL"],
-    "client_id": os.environ["FIREBASE_CLIENT_ID"],
+
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
     "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": os.environ["FIREBASE_CLIENT_CERT_URL"]
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs"
 }
 
 cred = credentials.Certificate(firebase_credentials)
